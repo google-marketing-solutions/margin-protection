68307c17844f208d19f9e338ba1e3e8e0e530437
gPS Team <no-reply@google.com>
2023-04-04 10:23:55 -0700

    Add rule for campaigns
    PiperOrigin-RevId: 521800020
    Change-Id: I4384b1b09e0163ad645107f5c8477d7ff766a94d

---
+++ sa360/src/rules.ts
@@ -0,0 +1,140 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {AbsoluteRule} from 'anomaly_library/absoluteRule';
+import {Rule, RuleInstructions, Value, Values} from 'anomaly_library/main';
+
+import {newRule} from './client';
+
+const ONE_DAY = 60 * 60 * 24;
+
+interface CampaignStatusData {
+  lastUpdated: number;
+  records: CampaignStatusDataRecords;
+}
+
+type CampaignStatusDataRecords = Array<[status: string, count: number]>;
+
+function toCampaignData(value: string): CampaignStatusDataRecords {
+  return value.split(',').map(v => {
+    const [status, count] = v.trim().split(' x ');
+    return [status, Number(count)];
+  });
+}
+
+function fromCampaignData(data: CampaignStatusDataRecords): string {
+  return data.map(([status, count]) => `${status} x ${count}`).join(', ');
+}
+
+/**
+ * Anomalous if campaign status has gone from inactive to active after N days.
+ *
+ * This is a somewhat complex rule because it matters how many days a value
+ * has been set, and this rule might be checked hourly or even ad-hoc.
+ */
+export const campaignStatusRule = newRule({
+  params: {
+    daysInactive: {
+      label: 'Max. Days Inactive before Active',
+    },
+  },
+  defaults: {
+    daysInactive: '0',
+  },
+  name: 'Campaign Status Active after Inactive',
+  uniqueKeyPrefix: 'campaignStatusCheck',
+  async callback() {
+    const uniqueKey: string = this.getUniqueKey();
+    const rules: {[campaignId: string]: Rule} = {};
+    const rule = this.getRule();
+    const values: Values = {};
+    const oldValues = rule.getValues().reduce((prev, value) => {
+      prev[value.fields!['Campaign ID']] = {lastUpdated: Number(value.fields!['Last Updated']), records: toCampaignData(value.value)};
+      return prev;
+    }, {} as {[key: string]: CampaignStatusData });
+
+    const campaignReport = await this.client.getCampaignReport();
+    const now = Date.now();
+    for (const [campaignId, reportRow] of Object.entries(campaignReport.report)) {
+      const valueArray = oldValues[campaignId] ?? { records: [], lastUpdated: now } as CampaignStatusData;
+      const moreThanOneDayOldStatus = now - valueArray.lastUpdated >= ONE_DAY;
+      const thresholdValue = Number(this.settings.getOrDefault(campaignId).daysInactive);
+      if (valueArray.records.length && valueArray.records[valueArray.records.length - 1][0] === reportRow.campaignStatus) {
+        // only add a value if the last status is more than one day old.
+        if (moreThanOneDayOldStatus) {
+          ++valueArray.records[valueArray.records.length - 1][1];
+        }
+      } else {
+        // remove the last value and replace it if the value is less than one
+        // day old.
+        if (!moreThanOneDayOldStatus && valueArray.records.length) {
+          if (valueArray.records[valueArray.records.length - 1][1] === 0) {
+            valueArray.records.pop();
+          } else {
+            --valueArray.records[valueArray.records.length - 1][1];
+          }
+        }
+        valueArray.records.push([reportRow.campaignStatus, 1]);
+        if (valueArray.records[0][0] !== 'Active') {
+          valueArray.records.shift();
+        }
+        // cull statuses with active/inactive/active under threshold
+        for (let i = 0; i < valueArray.records.length; i++) {
+          if (valueArray.records[i][0] === 'Active' && valueArray.records[i + 2] && valueArray.records[i + 1][0] === 'Paused' && valueArray.records[i + 1][1] <= thresholdValue && valueArray.records[i + 2][0] === 'Active') {
+            valueArray.records.splice(i, 2);
+          }
+        }
+      }
+      values[campaignId] = (rules[campaignId] ??= notActiveAfterPausedNDays({uniqueKey, thresholdValue})).createValue(fromCampaignData(valueArray.records), {
+        'Campaign ID': reportRow.campaignId,
+        'Campaign': reportRow.campaign,
+        'Current Status': reportRow.campaignStatus,
+        'Last Updated': String(valueArray.lastUpdated + (moreThanOneDayOldStatus ? ONE_DAY : 0)),
+      });
+    }
+    return { rule, values };
+  },
+});
+
+/**
+ * Given a series, this rule is anomalous when 'Active', 'Paused'*N, 'Active' is true.
+ *
+ * If 1 is never seen, if 1 is the only thing seen, or if 1 is only >N times,
+ * then this is not an error.
+ */
+export function notActiveAfterPausedNDays(instructions: RuleInstructions<number>) {
+  /**
+   * @param thresholdValue The number of times a false value should be seen to make this anomalous.
+   */
+  return new AbsoluteRule(instructions, (thresholdValue) => (value) => {
+    const valuesList = toCampaignData(value);
+    let clockStarted = false;
+    let maxConsecutiveFalse = -1;
+    for (let i = 0; i < valuesList.length; i++) {
+      const v = valuesList[i];
+      if (v[0] === 'Active') {
+        clockStarted = true;
+      } else if (v[0] === 'Paused') {
+        if (!clockStarted) {
+          continue;
+        }
+        maxConsecutiveFalse = Math.max(maxConsecutiveFalse, v[1]);
+      }
+    }
+    return clockStarted && maxConsecutiveFalse < 0 || maxConsecutiveFalse <= thresholdValue || valuesList[valuesList.length - 1][0] !== 'Active';
+  });
+}
